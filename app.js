fb813b0bc39a95cce1f21387995a5464d79b8634
unknown <ethanswafford@yahoo.com>
2022-03-17 14:35:45 -0500

    firstPush

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div');
+    const scoreDisplay = document.querySelector('span');
+    const startButton = document.querySelector('.start');
+
+    const width = 10 
+    let currentIndex = 0 // first div in the grid 
+    let appleIndex = 0 // first in the grid
+
+})
